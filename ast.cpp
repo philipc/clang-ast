26806c6dd2ec08da21cd867dca1ab7e883abdb5b
Philip Craig <philipjcraig@gmail.com>
2012-08-16 10:53:13 +1000

    Move ASTPrinter method definitions outside of class

--- ast.cpp
+++ ast.cpp
@@ -63,342 +63,51 @@ class ASTPrinter : public ASTConsumer, public RecursiveASTVisitor<ASTPrinter> {
 public:
   typedef RecursiveASTVisitor<ASTPrinter> VisitorBase;
 
-  explicit ASTPrinter(raw_ostream &OS, const ASTPrinterOptions &Options)
-    : Filter(*this, Options.FilterString), Context(NULL), OS(OS),
-      Indent(0), NeedNewline(false),
-      LastLocFilename(""), LastLocLine(~0U), Options(Options)
-  {}
+  explicit ASTPrinter(raw_ostream &OS, const ASTPrinterOptions &Options);
+  virtual void HandleTranslationUnit(ASTContext &Context);
 
-  virtual void HandleTranslationUnit(ASTContext &Context) {
-    this->Context = &Context;
-    Filter.TraverseDecl(Context.getTranslationUnitDecl());
-    this->Context = NULL;
-  }
+  bool TraverseDecl(Decl *D);
+  bool VisitDecl(Decl *D);
+  bool VisitVarDecl(VarDecl *D);
+  bool VisitFunctionDecl(FunctionDecl *D);
+  bool VisitLabelDecl(LabelDecl *D);
 
-  bool TraverseDecl(Decl *D) {
-    ++Indent;
-    bool Result = VisitorBase::TraverseDecl(D);
-    --Indent;
-    // Finish off the line now in case this was the top level.
-    // Currently only Decl can appear at the top level.
-    if (NeedNewline) {
-      OS << '\n';
-      NeedNewline = false;
-    }
-    return Result;
-  }
+  bool TraverseStmt(Stmt *S);
+  bool VisitStmt(Stmt *S);
+  bool VisitLabelStmt(LabelStmt *S);
+  bool VisitGotoStmt(GotoStmt *S);
 
-  bool VisitDecl(Decl *D) {
-    printIndent();
-    OS << D->getDeclKindName() << "Decl";
-    printSourceRange(D->getSourceRange());
-    // TODO: Attr
-    return true;
-  }
+  bool VisitPredefinedExpr(PredefinedExpr *E);
+  bool VisitIntegerLiteral(IntegerLiteral *E);
+  bool VisitFloatingLiteral(FloatingLiteral *E);
+  bool VisitStringLiteral(StringLiteral *E);
+  bool VisitCharacterLiteral(CharacterLiteral *E);
+  bool VisitUnaryOperator(UnaryOperator *E);
+  bool VisitUnaryExprOrTypeTraitExpr(UnaryExprOrTypeTraitExpr *E);
+  bool VisitCXXBoolLiteralExpr(CXXBoolLiteralExpr *E);
 
-  bool VisitVarDecl(VarDecl *D) {
-    StorageClass SC = D->getStorageClassAsWritten();
-    if (SC != SC_None)
-      OS << ' ' << VarDecl::getStorageClassSpecifierString(SC);
-    if (D->isThreadSpecified())
-      OS << " __thread";
-    if (D->isModulePrivate())
-      OS << " __module_private__";
+  bool TraverseType(QualType T);
+  bool VisitType(Type *T);
+  bool VisitBuiltinType(BuiltinType *T);
+  bool VisitRecordType(RecordType *T);
 
-    // TODO: more properties
+  bool TraverseTypeLoc(TypeLoc TL);
+  bool VisitTypeLoc(TypeLoc TL);
+  bool VisitQualifiedTypeLoc(QualifiedTypeLoc TL);
 
-    printIdentifier(D);
-    return true;
-  }
-
-  bool VisitFunctionDecl(FunctionDecl *D) {
-    StorageClass SC = D->getStorageClassAsWritten();
-    if (SC != SC_None)
-      OS << ' ' << VarDecl::getStorageClassSpecifierString(SC);
-    if (D->isInlineSpecified())
-      OS << " inline";
-    if (D->isVirtualAsWritten())
-      OS << " virtual";
-    if (D->isModulePrivate())
-      OS << " __module_private__";
-    // TODO: more properties
-    return true;
-  }
-
-  bool TraverseStmt(Stmt *S) {
-    ++Indent;
-    bool Result = VisitorBase::TraverseStmt(S);
-    --Indent;
-    return Result;
-  }
-
-  bool VisitStmt(Stmt *S) {
-    printIndent();
-    OS << S->getStmtClassName();
-    printSourceRange(S->getSourceRange());
-    return true;
-  }
-
-  bool VisitLabelDecl(LabelDecl *D) {
-    printIdentifier(D);
-    return true;
-  }
-
-  bool VisitLabelStmt(LabelStmt *S) {
-    printIdentifier(S->getDecl());
-    return true;
-  }
-
-  bool VisitGotoStmt(GotoStmt *S) {
-    printIdentifier(S->getLabel());
-    return true;
-  }
-
-  bool VisitPredefinedExpr(PredefinedExpr *E) {
-    switch (E->getIdentType()) {
-    default:
-      llvm_unreachable("unknown IdentType");
-    case PredefinedExpr::Func:
-      OS << " __func__";
-      break;
-    case PredefinedExpr::Function:
-      OS << " __FUNCTION__";
-      break;
-    case PredefinedExpr::LFunction:
-      OS << " L__FUNCTION__";
-      break;
-    case PredefinedExpr::PrettyFunction:
-      OS << " __PRETTY_FUNCTION__";
-      break;
-    }
-    return true;
-  }
-
-  bool VisitIntegerLiteral(IntegerLiteral *E) {
-    bool isSigned = E->getType()->isSignedIntegerType();
-    OS << ' ' << E->getValue().toString(10, isSigned);
-    return true;
-  }
-
-  bool VisitFloatingLiteral(FloatingLiteral *E) {
-    OS << ' ' << E->getValueAsApproximateDouble();
-    return true;
-  }
-
-  bool VisitStringLiteral(StringLiteral *E) {
-    OS << ' ';
-    E->outputString(OS);
-    return true;
-  }
-
-  bool VisitCharacterLiteral(CharacterLiteral *E) {
-    OS << ' ' << E->getValue();
-    return true;
-  }
-
-  bool VisitUnaryOperator(UnaryOperator *E) {
-    OS << ' ' << (E->isPostfix() ? "postfix" : "prefix")
-       << ' ' << UnaryOperator::getOpcodeStr(E->getOpcode());
-    return true;
-  }
-
-  bool VisitUnaryExprOrTypeTraitExpr(UnaryExprOrTypeTraitExpr *E) {
-    switch (E->getKind()) {
-    default:
-      llvm_unreachable("unknown IdentType");
-    case UETT_SizeOf:
-      OS << " sizeof";
-      break;
-    case UETT_AlignOf:
-      OS << " alignof";
-      break;
-    case UETT_VecStep:
-      OS << " vec_step";
-      break;
-    }
-    return true;
-  }
-
-  bool VisitCXXBoolLiteralExpr(CXXBoolLiteralExpr *E) {
-    OS << ' ' << (E->getValue() ? "true" : "false");
-    return true;
-  }
-
-  bool TraverseType(QualType T) {
-    ++Indent;
-    bool Result = VisitorBase::TraverseType(T);
-    --Indent;
-    return Result;
-  }
-
-  bool VisitType(Type *T) {
-    printIndent();
-    OS << T->getTypeClassName() << "Type";
-    return true;
-  }
-
-  bool VisitBuiltinType(BuiltinType *T) {
-    OS << ' ' << T->getName(Context->getPrintingPolicy());
-    return true;
-  }
-
-  bool VisitRecordType(RecordType *T) {
-    printIdentifier(T->getDecl());
-    return true;
-  }
-
-  bool TraverseTypeLoc(TypeLoc TL) {
-    ++Indent;
-    bool Result = VisitorBase::TraverseTypeLoc(TL);
-    --Indent;
-    return Result;
-  }
-
-  bool VisitTypeLoc(TypeLoc TL) {
-    printSourceRange(TL.getSourceRange());
-    return true;
-  }
-
-  bool VisitQualifiedTypeLoc(QualifiedTypeLoc TL) {
-    printSourceRange(TL.getSourceRange());
-    return true;
-  }
-
-  bool TraverseNestedNameSpecifier(NestedNameSpecifier *NNS) {
-    if (!NNS)
-      return true;
-
-    ++Indent;
-    printIndent();
-    OS << "NestedNameSpecifier ";
-    NNS->print(OS, Context->getPrintingPolicy());
-    bool Result = VisitorBase::TraverseNestedNameSpecifier(NNS);
-    --Indent;
-    return Result;
-  }
-
-  bool TraverseNestedNameSpecifierLoc(NestedNameSpecifierLoc NNS) {
-    if (!NNS)
-      return true;
-
-    ++Indent;
-    printIndent();
-    OS << "NestedNameSpecifier";
-    printSourceRange(NNS.getSourceRange());
-    OS << ' ';
-    NNS.getNestedNameSpecifier()->print(OS, Context->getPrintingPolicy());
-    bool Result = VisitorBase::TraverseNestedNameSpecifierLoc(NNS);
-    --Indent;
-    return Result;
-  }
-
-  bool TraverseDeclarationNameInfo(DeclarationNameInfo NameInfo) {
-    ++Indent;
-    printIndent();
-    OS << "DeclarationName";
-    printSourceRange(NameInfo.getSourceRange());
-    OS << ' ';
-    NameInfo.getName().printName(OS);
-    bool Result = VisitorBase::TraverseDeclarationNameInfo(NameInfo);
-    --Indent;
-    return Result;
-  }
-
-  bool TraverseConstructorInitializer(CXXCtorInitializer *Init) {
-    ++Indent;
-    printIndent();
-    OS << "CXXCtorInitializer ";
-    printSourceRange(Init->getSourceRange());
-    if (Init->isBaseInitializer())
-      TraverseTypeLoc(Init->getTypeSourceInfo()->getTypeLoc());
-    else if (Init->isAnyMemberInitializer())
-      printIdentifier(Init->getAnyMember());
-    if (Init->isWritten())
-      TraverseStmt(Init->getInit());
-    --Indent;
-    return true;
-  }
-
-  bool TraverseTemplateArgument(const TemplateArgument &Arg) {
-    ++Indent;
-    printIndent();
-    OS << "TemplateArgument";
-    bool Result = VisitorBase::TraverseTemplateArgument(Arg);
-    --Indent;
-    return Result;
-  }
-
-  bool TraverseTemplateArgumentLoc(const TemplateArgumentLoc &ArgLoc) {
-    ++Indent;
-    printIndent();
-    OS << "TemplateArgument";
-    printSourceRange(ArgLoc.getSourceRange());
-    bool Result = VisitorBase::TraverseTemplateArgumentLoc(ArgLoc);
-    --Indent;
-    return Result;
-  }
+  bool TraverseNestedNameSpecifier(NestedNameSpecifier *NNS);
+  bool TraverseNestedNameSpecifierLoc(NestedNameSpecifierLoc NNS);
+  bool TraverseDeclarationNameInfo(DeclarationNameInfo NameInfo);
+  bool TraverseConstructorInitializer(CXXCtorInitializer *Init);
+  bool TraverseTemplateArgument(const TemplateArgument &Arg);
+  bool TraverseTemplateArgumentLoc(const TemplateArgumentLoc &ArgLoc);
 
 private:
-  void printIndent() {
-    if (NeedNewline)
-      OS << '\n';
-    for (unsigned i = 1; i < Indent; ++i)
-      OS << "  ";
-    NeedNewline = true;
-  }
-
-  void printIdentifier(NamedDecl *D) {
-    ++Indent;
-    printIndent();
-    OS << "Identifier";
-    printLocation(D->getLocation());
-    OS << ' ' << D->getNameAsString();
-    --Indent;
-  }
-
-  void printLocation(SourceLocation Loc, bool PrintLine) {
-    // Based on StmtDumper::DumpLocation
-    SourceLocation SpellingLoc = Context->getSourceManager().getSpellingLoc(Loc);
-    PresumedLoc PLoc = Context->getSourceManager().getPresumedLoc(SpellingLoc);
-    if (!PLoc.isValid())
-      return;
-
-    const char *Filename = PLoc.getFilename();
-    unsigned Line = PLoc.getLine();
-    if (strcmp(LastLocFilename, Filename) != 0) {
-      OS << Filename << ':';
-      PrintLine = true;
-    }
-    if (PrintLine || LastLocLine != Line) {
-      OS << Line << ':';
-    }
-    OS << PLoc.getColumn();
-    LastLocFilename = Filename;
-    LastLocLine = Line;
-  }
-
-  void printLocation(SourceLocation Loc) {
-    if (!Options.EnableLoc)
-      return;
-
-    OS << " <";
-    printLocation(Loc, true);
-    OS << ">";
-  }
-
-  void printSourceRange(SourceRange R) {
-    if (!Options.EnableLoc)
-      return;
-
-    OS << " <";
-    printLocation(R.getBegin(), true);
-    if (R.getBegin() != R.getEnd()) {
-      OS << "-";
-      printLocation(R.getEnd(), false);
-    }
-    OS << ">";
-  }
-
+  void printIndent();
+  void printIdentifier(NamedDecl *D);
+  void printLocation(SourceLocation Loc, bool PrintLine);
+  void printLocation(SourceLocation Loc);
+  void printSourceRange(SourceRange R);
   ASTFilter<ASTPrinter> Filter;
   ASTContext *Context;
   raw_ostream &OS;
@@ -408,6 +117,341 @@ private:
   unsigned LastLocLine;
   const ASTPrinterOptions &Options;
 };
+
+ASTPrinter::ASTPrinter(raw_ostream &OS, const ASTPrinterOptions &Options)
+  : Filter(*this, Options.FilterString), Context(NULL), OS(OS),
+    Indent(0), NeedNewline(false),
+    LastLocFilename(""), LastLocLine(~0U), Options(Options)
+{}
+
+void ASTPrinter::HandleTranslationUnit(ASTContext &Context) {
+  this->Context = &Context;
+  Filter.TraverseDecl(Context.getTranslationUnitDecl());
+  this->Context = NULL;
+}
+
+bool ASTPrinter::TraverseDecl(Decl *D) {
+  ++Indent;
+  bool Result = VisitorBase::TraverseDecl(D);
+  --Indent;
+  // Finish off the line now in case this was the top level.
+  // Currently only Decl can appear at the top level.
+  if (NeedNewline) {
+    OS << '\n';
+    NeedNewline = false;
+  }
+  return Result;
+}
+
+bool ASTPrinter::VisitDecl(Decl *D) {
+  printIndent();
+  OS << D->getDeclKindName() << "Decl";
+  printSourceRange(D->getSourceRange());
+  // TODO: Attr
+  return true;
+}
+
+bool ASTPrinter::VisitVarDecl(VarDecl *D) {
+  StorageClass SC = D->getStorageClassAsWritten();
+  if (SC != SC_None)
+    OS << ' ' << VarDecl::getStorageClassSpecifierString(SC);
+  if (D->isThreadSpecified())
+    OS << " __thread";
+  if (D->isModulePrivate())
+    OS << " __module_private__";
+
+  // TODO: more properties
+
+  printIdentifier(D);
+  return true;
+}
+
+bool ASTPrinter::VisitFunctionDecl(FunctionDecl *D) {
+  StorageClass SC = D->getStorageClassAsWritten();
+  if (SC != SC_None)
+    OS << ' ' << VarDecl::getStorageClassSpecifierString(SC);
+  if (D->isInlineSpecified())
+    OS << " inline";
+  if (D->isVirtualAsWritten())
+    OS << " virtual";
+  if (D->isModulePrivate())
+    OS << " __module_private__";
+  // TODO: more properties
+  return true;
+}
+
+bool ASTPrinter::VisitLabelDecl(LabelDecl *D) {
+  printIdentifier(D);
+  return true;
+}
+
+bool ASTPrinter::TraverseStmt(Stmt *S) {
+  ++Indent;
+  bool Result = VisitorBase::TraverseStmt(S);
+  --Indent;
+  return Result;
+}
+
+bool ASTPrinter::VisitStmt(Stmt *S) {
+  printIndent();
+  OS << S->getStmtClassName();
+  printSourceRange(S->getSourceRange());
+  return true;
+}
+
+bool ASTPrinter::VisitLabelStmt(LabelStmt *S) {
+  printIdentifier(S->getDecl());
+  return true;
+}
+
+bool ASTPrinter::VisitGotoStmt(GotoStmt *S) {
+  printIdentifier(S->getLabel());
+  return true;
+}
+
+bool ASTPrinter::VisitPredefinedExpr(PredefinedExpr *E) {
+  switch (E->getIdentType()) {
+  default:
+    llvm_unreachable("unknown IdentType");
+  case PredefinedExpr::Func:
+    OS << " __func__";
+    break;
+  case PredefinedExpr::Function:
+    OS << " __FUNCTION__";
+    break;
+  case PredefinedExpr::LFunction:
+    OS << " L__FUNCTION__";
+    break;
+  case PredefinedExpr::PrettyFunction:
+    OS << " __PRETTY_FUNCTION__";
+    break;
+  }
+  return true;
+}
+
+bool ASTPrinter::VisitIntegerLiteral(IntegerLiteral *E) {
+  bool isSigned = E->getType()->isSignedIntegerType();
+  OS << ' ' << E->getValue().toString(10, isSigned);
+  return true;
+}
+
+bool ASTPrinter::VisitFloatingLiteral(FloatingLiteral *E) {
+  OS << ' ' << E->getValueAsApproximateDouble();
+  return true;
+}
+
+bool ASTPrinter::VisitStringLiteral(StringLiteral *E) {
+  OS << ' ';
+  E->outputString(OS);
+  return true;
+}
+
+bool ASTPrinter::VisitCharacterLiteral(CharacterLiteral *E) {
+  OS << ' ' << E->getValue();
+  return true;
+}
+
+bool ASTPrinter::VisitUnaryOperator(UnaryOperator *E) {
+  OS << ' ' << (E->isPostfix() ? "postfix" : "prefix")
+     << ' ' << UnaryOperator::getOpcodeStr(E->getOpcode());
+  return true;
+}
+
+bool ASTPrinter::VisitUnaryExprOrTypeTraitExpr(UnaryExprOrTypeTraitExpr *E) {
+  switch (E->getKind()) {
+  default:
+    llvm_unreachable("unknown IdentType");
+  case UETT_SizeOf:
+    OS << " sizeof";
+    break;
+  case UETT_AlignOf:
+    OS << " alignof";
+    break;
+  case UETT_VecStep:
+    OS << " vec_step";
+    break;
+  }
+  return true;
+}
+
+bool ASTPrinter::VisitCXXBoolLiteralExpr(CXXBoolLiteralExpr *E) {
+  OS << ' ' << (E->getValue() ? "true" : "false");
+  return true;
+}
+
+bool ASTPrinter::TraverseType(QualType T) {
+  ++Indent;
+  bool Result = VisitorBase::TraverseType(T);
+  --Indent;
+  return Result;
+}
+
+bool ASTPrinter::VisitType(Type *T) {
+  printIndent();
+  OS << T->getTypeClassName() << "Type";
+  return true;
+}
+
+bool ASTPrinter::VisitBuiltinType(BuiltinType *T) {
+  OS << ' ' << T->getName(Context->getPrintingPolicy());
+  return true;
+}
+
+bool ASTPrinter::VisitRecordType(RecordType *T) {
+  printIdentifier(T->getDecl());
+  return true;
+}
+
+bool ASTPrinter::TraverseTypeLoc(TypeLoc TL) {
+  ++Indent;
+  bool Result = VisitorBase::TraverseTypeLoc(TL);
+  --Indent;
+  return Result;
+}
+
+bool ASTPrinter::VisitTypeLoc(TypeLoc TL) {
+  printSourceRange(TL.getSourceRange());
+  return true;
+}
+
+bool ASTPrinter::VisitQualifiedTypeLoc(QualifiedTypeLoc TL) {
+  printSourceRange(TL.getSourceRange());
+  return true;
+}
+
+bool ASTPrinter::TraverseNestedNameSpecifier(NestedNameSpecifier *NNS) {
+  if (!NNS)
+    return true;
+
+  ++Indent;
+  printIndent();
+  OS << "NestedNameSpecifier ";
+  NNS->print(OS, Context->getPrintingPolicy());
+  bool Result = VisitorBase::TraverseNestedNameSpecifier(NNS);
+  --Indent;
+  return Result;
+}
+
+bool ASTPrinter::TraverseNestedNameSpecifierLoc(NestedNameSpecifierLoc NNS) {
+  if (!NNS)
+    return true;
+
+  ++Indent;
+  printIndent();
+  OS << "NestedNameSpecifier";
+  printSourceRange(NNS.getSourceRange());
+  OS << ' ';
+  NNS.getNestedNameSpecifier()->print(OS, Context->getPrintingPolicy());
+  bool Result = VisitorBase::TraverseNestedNameSpecifierLoc(NNS);
+  --Indent;
+  return Result;
+}
+
+bool ASTPrinter::TraverseDeclarationNameInfo(DeclarationNameInfo NameInfo) {
+  ++Indent;
+  printIndent();
+  OS << "DeclarationName";
+  printSourceRange(NameInfo.getSourceRange());
+  OS << ' ';
+  NameInfo.getName().printName(OS);
+  bool Result = VisitorBase::TraverseDeclarationNameInfo(NameInfo);
+  --Indent;
+  return Result;
+}
+
+bool ASTPrinter::TraverseConstructorInitializer(CXXCtorInitializer *Init) {
+  ++Indent;
+  printIndent();
+  OS << "CXXCtorInitializer ";
+  printSourceRange(Init->getSourceRange());
+  if (Init->isBaseInitializer())
+    TraverseTypeLoc(Init->getTypeSourceInfo()->getTypeLoc());
+  else if (Init->isAnyMemberInitializer())
+    printIdentifier(Init->getAnyMember());
+  if (Init->isWritten())
+    TraverseStmt(Init->getInit());
+  --Indent;
+  return true;
+}
+
+bool ASTPrinter::TraverseTemplateArgument(const TemplateArgument &Arg) {
+  ++Indent;
+  printIndent();
+  OS << "TemplateArgument";
+  bool Result = VisitorBase::TraverseTemplateArgument(Arg);
+  --Indent;
+  return Result;
+}
+
+bool ASTPrinter::TraverseTemplateArgumentLoc(const TemplateArgumentLoc &ArgLoc) {
+  ++Indent;
+  printIndent();
+  OS << "TemplateArgument";
+  printSourceRange(ArgLoc.getSourceRange());
+  bool Result = VisitorBase::TraverseTemplateArgumentLoc(ArgLoc);
+  --Indent;
+  return Result;
+}
+
+void ASTPrinter::printIndent() {
+  if (NeedNewline)
+    OS << '\n';
+  for (unsigned i = 1; i < Indent; ++i)
+    OS << "  ";
+  NeedNewline = true;
+}
+
+void ASTPrinter::printIdentifier(NamedDecl *D) {
+  ++Indent;
+  printIndent();
+  OS << "Identifier";
+  printLocation(D->getLocation());
+  OS << ' ' << D->getNameAsString();
+  --Indent;
+}
+
+void ASTPrinter::printLocation(SourceLocation Loc, bool PrintLine) {
+  // Based on StmtDumper::DumpLocation
+  SourceLocation SpellingLoc = Context->getSourceManager().getSpellingLoc(Loc);
+  PresumedLoc PLoc = Context->getSourceManager().getPresumedLoc(SpellingLoc);
+  if (!PLoc.isValid())
+    return;
+
+  const char *Filename = PLoc.getFilename();
+  unsigned Line = PLoc.getLine();
+  if (strcmp(LastLocFilename, Filename) != 0) {
+    OS << Filename << ':';
+    PrintLine = true;
+  }
+  if (PrintLine || LastLocLine != Line) {
+    OS << Line << ':';
+  }
+  OS << PLoc.getColumn();
+  LastLocFilename = Filename;
+  LastLocLine = Line;
+}
+
+void ASTPrinter::printLocation(SourceLocation Loc) {
+  if (!Options.EnableLoc)
+    return;
+
+  OS << " <";
+  printLocation(Loc, true);
+  OS << ">";
+}
+
+void ASTPrinter::printSourceRange(SourceRange R) {
+  if (!Options.EnableLoc)
+    return;
+
+  OS << " <";
+  printLocation(R.getBegin(), true);
+  if (R.getBegin() != R.getEnd()) {
+    OS << "-";
+    printLocation(R.getEnd(), false);
+  }
+  OS << ">";
+}
 
 class ASTPrinterAction : public ASTFrontendAction {
 public:
