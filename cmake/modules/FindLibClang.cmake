12e4f7bd78c7fe8c4bcf086230ad524cafd42f6a
Philip Craig <philipjcraig@gmail.com>
2012-09-19 22:32:14 +1000

    Use the installed clang for standalone builds
    For the standalone build, link against the installed version of clang,
    rather than having hardcoded paths to the build tree.

---
+++ cmake/modules/FindLibClang.cmake
@@ -0,0 +1,20 @@
+execute_process(
+  COMMAND llvm-config --cxxflags
+  OUTPUT_VARIABLE LibClang_DEFINITIONS)
+
+set(LibClang_DEFINITIONS ${LibClang_DEFINITIONS} "-fno-rtti")
+
+execute_process(
+  COMMAND llvm-config --includedir
+  OUTPUT_VARIABLE LibClang_INCLUDE_DIR)
+
+find_library(LibClang_LIBRARY NAMES clang)
+find_library(LibClangTooling_LIBRARY NAMES clangTooling)
+
+set(LibClang_LIBRARIES ${LibClangTooling_LIBRARY} ${LibClang_LIBRARY})
+set(LibClang_INCLUDE_DIRS ${LibClang_INCLUDE_DIR})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LibClang DEFAULT_MSG LibClang_LIBRARY LibClang_INCLUDE_DIR)
+
+mark_as_advanced(LibClang_INCLUDE_DIR LibClang_LIBRARY)
